56313f60972b5103afad5767630931c92b0dc9ee
Roberto Sora <r.sora@arduino.cc>
2022-06-26 19:43:59 +0200

    Add 2 syllable append

--- cmd/tsn/main.go
+++ cmd/tsn/main.go
@@ -6,7 +6,13 @@ import (
 	"github.com/rsora/tsn/internal/syllable"
 )
 
+const syllables = 2
+
 func main() {
-	s := syllable.New()
+	var s = ""
+	for i := 0; i < syllables; i++ {
+		s = fmt.Sprint(s, syllable.New())
+	}
+
 	fmt.Println(s)
 }
